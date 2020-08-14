29aa73fcf5853bfe1b97e12010c8bca03301a8e9
HoiPh <winwin260299@gmail.com>
2020-08-14 22:52:23 +0700

    datepicker_2

--- resources/views/client/course/meet.blade.php
+++ resources/views/client/course/meet.blade.php
@@ -121,6 +121,7 @@
         </form>
     </div>
     <!-- partial -->
+    <script defer src="{{ asset('client/js/all.js') }}"></script> <!--load all styles -->
   <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js'></script>
 <script src='https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js'></script>
 <script src='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js'></script>
